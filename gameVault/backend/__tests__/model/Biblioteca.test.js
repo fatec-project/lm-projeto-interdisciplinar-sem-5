7d68eedec3f28de206eb2e046247e54ad1335580
marcelo-almeida-de-araujo <almeidadearaujomarcelo@gmail.com>
2025-06-12 13:51:46 -0300

    Cria arquivo de teste da classe de entidade Biblioteca

---
+++ gameVault/backend/__tests__/model/Biblioteca.test.js
@@ -0,0 +1,20 @@
+const Biblioteca = require('../../model/Biblioteca');
+
+describe('A classe de entidade Biblioteca', () => {
+  const biblioteca = new Biblioteca;
+
+  it('Deve possuir métodos getter e setter de usuarioId', () => {
+    biblioteca.setUsuarioId(1);
+    expect(biblioteca.getUsuarioId()).toBe(1);
+  });
+
+  it('Deve possuir métodos getter e setter de jogoId', () => {
+    biblioteca.setJogoId(1);
+    expect(biblioteca.getJogoId()).toBe(1);
+  });
+
+  it('Deve possuir métodos getter e setter de foiPresente', () => {
+    biblioteca.setFoiPresente(false);
+    expect(biblioteca.getFoiPresente()).toBe(false);
+  });
+});
