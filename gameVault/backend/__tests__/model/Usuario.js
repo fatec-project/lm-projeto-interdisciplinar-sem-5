7a58fa8014af458aa7575c89be1ab2c2c0c0d1c7
marcelo-almeida-de-araujo <almeidadearaujomarcelo@gmail.com>
2025-06-12 13:19:10 -0300

    Adiciona os testes de unidade restantes dos métodos da entidade Usuario

--- gameVault/backend/__tests__/model/Usuario.js
+++ gameVault/backend/__tests__/model/Usuario.js
@@ -7,4 +7,19 @@ describe('A classe de entidade Usuario', () => {
     usuario.setId(1);
     expect(usuario.getId()).toBe(1);
   });
+
+  it('Deve possuir métodos getter e setter de Nome', () => {
+    usuario.setNome('Verso');
+    expect(usuario.getNome()).toBe('Verso');
+  });
+
+  it('Deve possuir métodos getter e setter de Email', () => {
+    usuario.setEmail('maelle@email.com');
+    expect(usuario.getEmail()).toBe('maelle@email.com');
+  });
+
+  it('Deve possuir métodos getter e setter de Senha', () => {
+    usuario.setSenha('abcd1234!');
+    expect(usuario.getSenha()).toBe('abcd1234!');
+  });
 });
