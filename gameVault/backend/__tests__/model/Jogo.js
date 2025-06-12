e4f27ac7a27191de3f7847ea87c2d650dcaade0d
marcelo-almeida-de-araujo <almeidadearaujomarcelo@gmail.com>
2025-06-12 13:48:21 -0300

    Cria arquivo de teste da classe de entidade Jogo

---
+++ gameVault/backend/__tests__/model/Jogo.js
@@ -0,0 +1,10 @@
+const Jogo = require('../../model/Jogo');
+
+describe('A classe de entidade Jogo', () => {
+  const jogo = new Jogo;
+
+  it('Deve possuir métodos getter e setter de Id', () => {
+    jogo.setId(1);
+    expect(jogo.getId()).toBe(1);
+  });
+});
