63cd7cd3681d2a473becaf1fd5bec0d111e970a7
marcelo-almeida-de-araujo <almeidadearaujomarcelo@gmail.com>
2025-06-12 14:09:26 -0300

    Cria arquivo de teste da classe de entidade Amigo

---
+++ gameVault/backend/__tests__/model/Amigo.test.js
@@ -0,0 +1,15 @@
+const Amigo = require('../../model/Amigo');
+
+describe('A classe de entidade Amigo', () => {
+  const amigo = new Amigo;
+
+  it('Deve possuir métodos getter e setter de usuario1Id', () => {
+    amigo.setUsuario1Id(1);
+    expect(amigo.getUsuario1Id()).toBe(1);
+  });
+
+  it('Deve possuir métodos getter e setter de usuario2Id', () => {
+    amigo.setUsuario2Id(2);
+    expect(amigo.getUsuario2Id()).toBe(2);
+  });
+});
